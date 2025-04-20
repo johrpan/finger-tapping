7b2d7b3267c4a771c51fc369fcfcecbb48e0462f
Elias Projahn <elias@johrpan.de>
2025-04-20 19:40:50 +0200

    Add CD to GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,10 @@ export default {
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'development' ? '' : '/finger-tapping',
+		}
+
 	}
 };
